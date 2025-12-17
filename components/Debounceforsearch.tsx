e59272ed851c16934f2522ca93683b9b3981c221
Manish Karki <65812794+Manish-Karke@users.noreply.github.com>
2025-12-17 18:25:03 +0545

    cache system from apollo

--- components/Debounceforsearch.tsx
+++ components/Debounceforsearch.tsx
@@ -1,15 +1,12 @@
 import debounce from "lodash.debounce";
-import { searchCache, searchVar} from "./localState/cache"
+import { searchCache, searchVar } from "./localState/cache";
 import { SEARCH_PRODUCT } from "./localState/localQuery";
 
-export const debouncedWriteSearch = debounce(
-  (text: string) => {
-    searchCache.writeQuery({
-      query: SEARCH_PRODUCT,
-      data: {
-        searchText: searchVar(text),
-      },
-    });
-  },
-  
-);
+export const debouncedWriteSearch = debounce((text: string) => {
+  searchCache.writeQuery({
+    query: SEARCH_PRODUCT,
+    data: {
+      searchText: searchVar(text),
+    },
+  });
+}, 500);
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -1,6 +1,9 @@
 import { AntDesign } from "@expo/vector-icons";
-import React from "react";
+import debounce from "lodash.debounce";
+import React, { useState } from "react";
 import { TextInput, View } from "react-native";
+import { searchCache, searchVar } from "./localState/cache";
+import { SEARCH_PRODUCT } from "./localState/localQuery";
 
 type SearchInputProps = {
   value: string;
@@ -13,17 +16,27 @@ export function SearchInput({
   onChange,
   placeholder = "Search...",
 }: SearchInputProps) {
-  return (
-   <View className="flex-row px-4 py-3 items-center m-2 border border-gray-300 rounded-lg bg-slate-400">
-  <AntDesign name="search" size={24} color="gray" className="mr-2" />
-  <TextInput
-    value={value}
-    placeholder={placeholder}
-    onChangeText={onChange}
-    className="flex-1 h-7"
-    maxFontSizeMultiplier={2}
-  />
-</View>
+  const [searchValue, setSearchValue] = useState("");
 
+  const handleSearchValue = (value: string) => {
+    setSearchValue(value);
+    searchCache.writeQuery({
+      query: SEARCH_PRODUCT,
+      data: {
+        searchText: searchVar(value),
+      },
+    });
+  };
+  return (
+    <View className="flex-row px-4 py-3 items-center m-2 border border-gray-300 rounded-lg bg-slate-400">
+      <AntDesign name="search" size={24} color="gray" className="mr-2" />
+      <TextInput
+        value={searchValue}
+        placeholder={placeholder}
+        onChangeText={handleSearchValue}
+        className="flex-1 h-7"
+        maxFontSizeMultiplier={2}
+      />
+    </View>
   );
 }
